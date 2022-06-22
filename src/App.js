f450efc2575a42d76130f452b76826d7367d947e
K0st3k6969 <p.romanczuk@gmail.com>
2022-06-22 17:45:16 +0200

    Added Header component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./components/App.css"
+import React from "react";
+import Header from "./components/Header"
+import AddContact from "./components/AddContact"
+import ContactCard from "./components/ContactCard"
+import ContactList from "./components/ContactList"
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      {/* <AddContact />
+      <ContactCard />
+      <ContactList /> */}
     </div>
   );
 }
+
+
 
 export default App;
